c408cc7b4882fcc6ae759693df5706adbcc39d7b
RadyaI <radyaiftikhar@gmail.com>
2025-01-21 20:34:09 +0700

    Update: login content component

--- src/login.jsx
+++ src/login.jsx
@@ -5,7 +5,11 @@ export default function Login() {
     return (
         <>
             <Navbar></Navbar>
-            <Container></Container>
+            <Container>
+                <Content>
+                    Welcome to login simulation
+                </Content>
+            </Container>
         </>
     )
 }
@@ -14,4 +18,14 @@ const Container = styled.div`
     width: 100%;
     height: 100dvh;
     background-color: #03001C;
+    overflow-y: auto;
+`
+
+const Content = styled.div`
+    border: 1px solid white;
+    color: white;
+    width: 90%;
+    height: 200px;
+    margin: 0 auto;
+    margin-top: 100px;
 `
