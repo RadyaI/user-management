a1ef5b26d5dcba4f4df245af46b68d9187ce1276
RadyaI <radyaiftikhar@gmail.com>
2025-01-27 20:23:13 +0700

    tes: webkit scrollbar

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,72 +1,75 @@
-import { useEffect, useState } from "react"
 import styled from "styled-components"
 
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-
 export default function Auth() {
-
-    // const [display, setDisplay] = useState("login")
-    useEffect(() => {
-        new Swiper('.swiper', {
-            // Optional parameters
-            direction: 'horizontal',
-            loop: true,
-
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-            },
-
-            // Navigation arrows
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-
-            // And if we need scrollbar
-            // scrollbar: {
-            //     el: '.swiper-scrollbar',
-            // },
-        });
-    }, [])
-
     return (
         <>
-            <div className="swiper" style={{ width: "600px", height: "300px" }}>
-                <div className="swiper-wrapper">
-                    <div className="swiper-slide">Slide 1</div>
-                    <div className="swiper-slide">Slide 2</div>
-                    <div className="swiper-slide">Slide 3</div>
-                    ...
-                </div>
-                <div className="swiper-pagination"></div>
+            <Container>
 
-                <div className="swiper-button-prev"></div>
-                <div className="swiper-button-next"></div>
-            </div>
+                <div className="wrapper">
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                </div>
+
+            </Container>
         </>
     )
 }
 
 const Container = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 50%;
-    height: 50dvh;
-    border: 1px solid black;
-`
-
-const Login = styled.div`
-    background-color: yellow;
     width: 100%;
-    height: 100%;
-`
+    height: 100dvh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-const Register = styled.div`
-    background-color: yellow;
-    width: 100%;
-    height: 100%;
+    .wrapper{
+        border: 1px solid black;
+        width: 50%;
+        height: 50%;
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        overflow-y: auto;
+    }
+
+    .wrapper .card{
+        background-color: black;
+        width: 100px;
+        height: 50px;
+        margin: 5px;
+        flex-shrink: 0;
+    }
+
+    .wrapper .card:nth-child(1){
+        background-color: blue;
+    }
+
+    .wrapper .card:nth-child(13){
+        background-color: yellow;
+    }
+
+    .wrapper::-webkit-scrollbar{
+        height: 10px;
+    }
+
+    .wrapper::-webkit-scrollbar-thumb{
+        background-color: green;
+        border-radius: 50px;
+    }
+
+    .wrapper::-webkit-scrollbar-track{
+        display: none;
+    }
+
 `
